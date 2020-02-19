aaf290fe92781c43800414f0febb1f6cd1a29b0f
Bhuvana Jothivel <58466121+BhuvanaJothivel@users.noreply.github.com>
2020-02-19 16:16:13 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,156 +1,43 @@
-//Trial 1:
+//Function 1:
 //use slice method in a given array
+//If array contains certain elements, and if slice method contains index value as negative then return 'Invalid'
+//If slice method contains valid index value, it should return output according to the index value given in slice method
+//syntax for slice is slice(index1, index2)
 
 
-//Trial 2:
+//Function 2:
 //use splice method in a given array
+//If array contains certain elements, and if splice method contains index value as negative then return 'Invalid'
+//If splice method contains valid index value, it should return output according to the index value given in splice method
+//If splice method contains empty string values or null, it should return 'Invalid'
+//syntax for slice is slice(index1, index2, 'string1','string2')
 
-//Trial 3:
+//Function 3:
 //Filter functions
 //create filterIsEven and filterIsPrime functions and implement the filter method in it
 //For example if number is an array of elements. Then, number.filter(isEven) and number.filter(isPrime)
+//This method also checks whether array contains nothing, or it contains only negative values or it contains any strings and returns 'Invalid'
 
-// /Trial 4:
-// Reject functions
+//Function 4:
+//Reject functions
 //create rejectNonPrime functions and implement the reject method in it
 //For example if number is an array of elements. Then, number.reject(nonPrime)
+//This method also checks whether array contains nothing, or it contains only negative values or it contains any strings and returns 'Invalid'
 
-//Trial 5:
+//Function 5:
 //Lambda functions
 //create isEven functions by using lambda implementation
+//This method also checks whether array contains nothing, or it contains only negative values or it contains any strings and returns 'Invalid'
 
-//Trial 6:
+//Function 6:
 //Map functions
 //create findSquareOfNumber functions and implement the map method in it
 //For example if number is an array of elements. Then, number.map(findSquareOfNumber)
+//This method also checks whether array contains nothing, or it contains only negative values or it contains any strings and returns 'Invalid'
 
-//Trial 7:
+//Function 7:
 //Reduce functions
 //create multiply functions and implement the reduce method in it
+//This method also checks whether array contains nothing, or it contains only negative values or it contains any strings and returns 'Invalid'
 
 
-function filterIsEven(numbers){
-    const x = new Array();
-    var count = 0;
-    if(numbers.length === 0)
-        return 'Invalid';
-    for(var i=0; i<numbers.length;i++){
-        if(numbers[i]<0)
-            count++; 
-    }
-    if(count === numbers.length)
-        return 'Invalid';
-    for(var i=0; i<numbers.length;i++){
-        if(numbers[i]%2===0)
-            x.push(numbers[i]);
-    }
-    return x;
-}
-  
-function isEven(numbers){
-    const x = new Array();
-    var count = 0;
-    if(numbers.length === 0)
-        return 'Invalid';
-    for(var i=0; i<numbers.length;i++){
-        if(numbers[i]<0)
-            count++; 
-    }
-    if(count === numbers.length)
-        return 'Invalid';
-    for(var i=0; i<numbers.length;i++){
-        if(numbers[i]%2===0)
-            x.push(numbers[i]);
-    }
-    return x;
-}
-  
-
-function filterIsPrime(numbers){
-    const y = new Array();
-    var count = 0;
-    if(numbers.length === 0)
-        return 'Invalid';
-    for(var i=0; i<numbers.length;i++){
-        if(numbers[i]<0)
-            count++; 
-    }
-    if(count === numbers.length)
-        return 'Invalid';
-    for(var i=0; i<numbers.length;i++){
-        var temp = true;
-        if(numbers[i] === 1 || numbers[i] === 2)
-            y.push(numbers[i]);
-        else{
-        for(var j=2; j<numbers[i]; j++){
-            if(numbers[i]%j===0){
-                temp = false;
-                break;
-            }
-        }
-        if(temp)
-            y.push(numbers[i]);
-    }
-    }
-    return y;
-}
-  
-function rejectNonPrime(numbers){
-    const y = new Array();
-    var count = 0;
-    if(numbers.length === 0)
-        return 'Invalid';
-    for(var i=0; i<numbers.length;i++){
-        if(numbers[i]<0)
-            count++; 
-    }
-    if(count === numbers.length)
-        return 'Invalid';
-    for(var i=0; i<numbers.length;i++){
-        if(numbers[i] === 1 || numbers[i] === 2)
-            continue;
-        else{
-            for(var j=2; j<numbers[i]; j++){
-                if(numbers[i]%j===0){
-                    y.push(numbers[i]);
-                }
-            }
-        }  
-    } 
-    return y;
-}
-
-function findSquareOfNumbers(numbers){
-    const y = new Array();
-    var count = 0;
-    if(numbers.length === 0)
-        return 'Invalid';
-    for(var i=0; i<numbers.length;i++){
-        if(numbers[i]<0)
-            count++; 
-    }
-    if(count === numbers.length)
-        return 'Invalid';
-    for(var i=0; i<numbers.length;i++){
-        var temp = Math.pow(numbers[i],2);
-        y.push(temp);
-    } 
-    return y;
-}
-
-function multiply(numbers){
-    var count = 0;
-    if(numbers.length === 0)
-        return 'Invalid';
-    for(var i=0; i<numbers.length;i++){
-        if(numbers[i]<0)
-            count++; 
-    }
-    if(count === numbers.length)
-        return 'Invalid';
-    var temp = 1;
-    for(var i=0; i<numbers.length;i++){
-        temp = temp * numbers[i];  
-    } 
-    return temp;
-}
